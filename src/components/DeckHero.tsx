785f62469e5b7cbe00db64735624aa6bf723446e
fibr <fabry.d@seznam.cz>
2021-12-19 17:17:33 +0100

    presunuti logiky do Game rodicovske komponenty

--- src/components/DeckHero.tsx
+++ src/components/DeckHero.tsx
@@ -1,50 +1,22 @@
 import { sortBy } from 'lodash'
 import React, { useMemo } from 'react'
-import {
-	clearBoard,
-	opponentActs,
-	removeCard,
-	selectCards,
-} from '../store/slices/cardsSlice'
-import { useAppDispatch, useAppSelector } from '../store/hooks'
+import { selectCards } from '../store/slices/cardsSlice'
+import { useAppSelector } from '../store/hooks'
 import { selectCurrentScore } from '../store/slices/scoreSlice'
 import { Card } from '../types'
-import { delay } from '../utils'
-import {
-	disableActing,
-	enableActing,
-	selectCanHeroAct,
-} from '../store/slices/commonSlice'
 
-export const DeckHero = () => {
-	const dispatch = useAppDispatch()
+interface DeckHeroProps {
+	onClick: (card: Card) => void
+}
+
+export const DeckHero = ({ onClick }: DeckHeroProps) => {
 	const isSettingsSorted = true
-	const carHeroAct = useAppSelector(selectCanHeroAct)
 	const cards = useAppSelector(selectCards)
 	const currentScore = useAppSelector(selectCurrentScore)
 	const heroCards = useMemo(
 		() => (isSettingsSorted ? sortBy(cards[3], ['id']) : cards[3]),
 		[cards, isSettingsSorted]
 	)
-
-	async function allOpponentsAct() {
-		for (const i of [0, 1, 2]) {
-			await delay()
-			dispatch(opponentActs(i))
-		}
-	}
-
-	async function handleClick(card: Card) {
-		if (!carHeroAct) return
-
-		dispatch(clearBoard())
-		dispatch(removeCard({ playerIndex: 3, card }))
-		dispatch(disableActing())
-		await allOpponentsAct()
-		await delay(1000)
-		dispatch(enableActing())
-		dispatch(clearBoard())
-	}
 
 	return (
 		<div className="hero-wrapper">
@@ -58,7 +30,7 @@ export const DeckHero = () => {
 						className="card card--hero"
 						src={card.src}
 						alt={card.name}
-						onClick={() => handleClick(card)}
+						onClick={() => onClick(card)}
 					/>
 				))}
 			</div>
--- src/views/Game.tsx
+++ src/views/Game.tsx
@@ -1,17 +1,51 @@
 import React, { useEffect } from 'react'
 import { DeckHero } from '../components/DeckHero'
-import { shuffleCards } from '../store/slices/cardsSlice'
-import { useAppDispatch } from '../store/hooks'
+import {
+	clearBoard,
+	opponentActs,
+	removeCard,
+	shuffleCards,
+} from '../store/slices/cardsSlice'
+import { useAppDispatch, useAppSelector } from '../store/hooks'
 import { DeckOpponent } from '../components/DeckOpponent'
 import { Board } from '../components/Board'
 import { setPlayerNames } from '../store/slices/nameSlice'
+import { Card } from '../types'
+import {
+	disableActing,
+	enableActing,
+	selectCanHeroAct,
+} from '../store/slices/commonSlice'
+import { delay } from '../utils'
 
 export const Game = () => {
 	const dispatch = useAppDispatch()
+	const carHeroAct = useAppSelector(selectCanHeroAct)
+
 	useEffect(() => {
 		dispatch(shuffleCards())
 		dispatch(setPlayerNames())
 	}, [dispatch])
+
+	async function allOpponentsAct() {
+		for (const i of [0, 1, 2]) {
+			await delay()
+			dispatch(opponentActs(i))
+		}
+	}
+
+	async function handleHeroClick(card: Card) {
+		if (!carHeroAct) return
+
+		dispatch(clearBoard())
+		dispatch(removeCard({ playerIndex: 3, card }))
+		dispatch(disableActing())
+		await allOpponentsAct()
+		await delay(1000)
+		dispatch(enableActing())
+		dispatch(clearBoard())
+	}
+
 	return (
 		<div className="game-area">
 			<div className="opponents-wrapper">
@@ -20,7 +54,7 @@ export const Game = () => {
 				))}
 			</div>
 			<Board />
-			<DeckHero />
+			<DeckHero onClick={handleHeroClick} />
 		</div>
 	)
 }
